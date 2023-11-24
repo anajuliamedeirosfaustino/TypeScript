ef88d7ac1be762c534c07148aa2485b8842c667a
Ana Júlia Medeiros <julinhamedeiros2013@gmail.com>
2023-11-24 19:22:29 -0300

    Códigos utilizando TypeScript

---
+++ 01variaveis.js
@@ -0,0 +1,8 @@
+var nome = 'Camila';
+var idade = 26;
+var estuda = true;
+var endereco = 'Centro';
+console.log("O nome \u00E9 ".concat(nome));
+console.log("A idade \u00E9 ".concat(idade));
+console.log("Estuda ".concat(estuda));
+console.log("O endereco \u00E9 ".concat(endereco));
